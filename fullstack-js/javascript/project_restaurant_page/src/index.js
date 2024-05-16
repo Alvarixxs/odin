6bc2f1583981ea01528c8b76ea70a20100c27aee
alvarixxs <alvarodesants@gmail.com>
2024-05-16 23:35:13 +0200

    kd

--- fullstack-js/javascript/project_restaurant_page/src/index.js
+++ fullstack-js/javascript/project_restaurant_page/src/index.js
@@ -1,4 +1,27 @@
 import './style.css';
+import {createHome} from "./home/home";
+import {createAbout} from "./about/about";
+import {createMenu} from "./menu/menu";
 
-console.log('hello')
+const content = document.getElementById('content')
 
+const home = createHome()
+content.appendChild(home)
+
+const homeButton = document.getElementById('home')
+homeButton.addEventListener('click', () => {
+    content.removeChild(content.firstChild)
+    content.appendChild(createHome())
+})
+
+const menuButton = document.getElementById('menu')
+menuButton.addEventListener('click', () => {
+    content.removeChild(content.firstChild)
+    content.appendChild(createMenu())
+})
+
+const aboutButton = document.getElementById('about')
+aboutButton.addEventListener('click', () => {
+    content.removeChild(content.firstChild)
+    content.appendChild(createAbout())
+})
